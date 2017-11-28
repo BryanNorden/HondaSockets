ea7ecd8aa3d9ddb94e7ab4009513a3c5f9679ca2
Chris Loncarich <loncarichchris@gmail.com>
2017-11-27 18:51:14 -0800

    initiate socket

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-
 var app = express();
+var io = require('socket.io')(app);
+
 app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
-app.get('/report', function(req, res) {
+
+io.on('connection', function(socket){
+  socket.on('accidentReport', function(report){
+    io.emit('newAccident', report);
+  });
+  console.log('a user connected');
+});
+
+app.post('/report', function(req, res) {
   console.log('!!!GTREAT SUCCESS')
   res.send(200);
 })
